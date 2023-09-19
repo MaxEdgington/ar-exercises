b5bde1d3b9f5ccc832d5dc17735d6aba7200f82e
Max Edgington <edgington.m.w@gmail.com>
2023-09-19 14:34:28 -0400

    add validations to store and employee, and create a a user prompt to add a failing user prompt to add a store and display validation error messages

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,6 +5,21 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
 puts "Exercise 6"
 puts "----------"
 
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+
+puts "Please enter a store"
+store_name = gets.chomp
+
+# Create a new store with just the name
+new_store = Store.new(name: store_name)
+
+# Attempt to save the store
+if new_store.save
+  puts "Store successfully created."
+else
+  puts "Failed to create store. Errors:"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
