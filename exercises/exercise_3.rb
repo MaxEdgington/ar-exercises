4819d085ea399637e05f024697f92fa4583b6be2
Max Edgington <edgington.m.w@gmail.com>
2023-09-19 12:38:05 -0400

    exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,15 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
 puts "Exercise 3"
-puts "----------"
+puts @store3.name
+puts Store.count
 
 # Your code goes here ...
+
+require_relative '../setup'
+require_relative './exercise_1'
+
